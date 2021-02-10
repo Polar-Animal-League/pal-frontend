e48424e1b2b775e3823299907480751c6137e07d
Michał Isalski <micha_i@interia.pl>
2021-02-10 08:44:44 +0100

    added prettier (#2)
    changed the default port to 80
    added missing slash in ROOT var
    updated the shared lib version
    added an improved dev Dockerfile
    removed the bracket style rule

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,25 @@ import * as mollify from 'mollify';
 import * as http from 'http';
 import { Express } from 'express-serve-static-core';
 import * as express from 'express';
+import * as chalk from 'chalk';
+
+process.on('SIGINT', () => process.exit(1));
 
 const app: Express = express();
 const server = http.createServer(app);
 
 const port = +(process.env['PORT'] ?? '1337');
 const ip = process.env['IP'] ?? '0.0.0.0';
-const rootPath = __dirname + process.env['ROOT'] ?? 'www';
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-app.use(mollify({
-    dir: rootPath,
-    is: true // default
-}));
+const rootPath = __dirname + process.env['ROOT'] ?? '/www';
+app.use(
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    mollify({
+        dir: rootPath,
+        is: true // default
+    })
+);
 
 app.use(express.static(rootPath));
 
 server.listen(port, ip);
+console.log(chalk.green(`Server listening on port ${port}`));
--- src/types/mollify/index.d.ts
+++ src/types/mollify/index.d.ts
@@ -1,3 +1,6 @@
+declare function mollify(options: {
+    dir: string;
+    is: boolean;
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
-declare function mollify(options: {dir: string, is: boolean}): import('express-serve-static-core').PathParams;
+}): import('express-serve-static-core').PathParams;
 declare module 'mollify';
